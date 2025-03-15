4bd297121c065142cbd956f0d934f90d746eb3ce
tsnadmin <121678992+mtsn22@users.noreply.github.com>
2025-03-15 15:46:15 +0700

    tailwind-scrollbar

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/filament/admin/theme.css',
+                'resources/css/filament/tsn/theme.css',
+                'resources/css/filament/walisantri/theme.css',
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'vendor/tomatophp/filament-simple-theme/resources/css/theme.css'
